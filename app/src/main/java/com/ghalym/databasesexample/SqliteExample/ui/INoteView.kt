32f8ee28241c29a30f5667fd614f948d30336120
Mahmoud Jamal Abu Ghali <abughali@innosoft.me>
2020-04-07 23:02:55 +0200

    create INoteView contains function will change the view

--- app/src/main/java/com/ghalym/databasesexample/SqliteExample/ui/INoteView.kt
+++ app/src/main/java/com/ghalym/databasesexample/SqliteExample/ui/INoteView.kt
@@ -1,2 +1,14 @@
 package com.ghalym.databasesexample.SqliteExample.ui
 
+import com.ghalym.databasesexample.SqliteExample.ICommonView
+import com.ghalym.databasesexample.SqliteExample.data.Note
+
+interface INoteView : ICommonView {
+
+    fun onAddSuccess();
+    fun onDeletSuccess();
+    fun onEditSuccess();
+    fun onDeleteAllSuccess();
+    fun onFetchData(result: ArrayList<Note>);
+
+}
