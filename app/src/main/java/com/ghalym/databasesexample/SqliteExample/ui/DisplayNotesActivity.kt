1ab2f99e191a1f516447fbc03da635a833c0ece1
Mahmoud Jamal Abu Ghali <abughali@innosoft.me>
2020-04-07 23:43:11 +0200

    implement addNote xml views and implement Note Activity(move to AddActivity and DisplayNotesActivity)

---
+++ app/src/main/java/com/ghalym/databasesexample/SqliteExample/ui/DisplayNotesActivity.kt
@@ -0,0 +1,13 @@
+package com.ghalym.databasesexample.SqliteExample.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ghalym.databasesexample.R
+
+class DisplayNotesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_notes)
+    }
+}
--- app/src/main/java/com/ghalym/databasesexample/SqliteExample/ui/NoteActivity.kt
+++ app/src/main/java/com/ghalym/databasesexample/SqliteExample/ui/NoteActivity.kt
@@ -1,13 +1,28 @@
 package com.ghalym.databasesexample.SqliteExample.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ghalym.databasesexample.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class NoteActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnGoToAddNote.setOnClickListener {
+
+            val intent = Intent(this, AddNoteActivity::class.java);
+            startActivity(intent)
+        }
+
+
+        btnGoToDisplayNotes.setOnClickListener {
+
+            val intent = Intent(this, AddNoteActivity::class.java);
+            startActivity(intent)
+        }
     }
 }
