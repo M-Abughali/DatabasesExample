48049a0f4148ace41d106502ad030c98386ca31f
Mahmoud Jamal Abu Ghali <abughali@innosoft.me>
2020-04-07 23:27:32 +0200

    changed the DbConnection class name to be Note Repository, Remove INote Presenter becuase there is no benifit from it , create all Note Presenter functions (connect to Repesotry and inser-delete-select-update note).

--- app/src/main/java/com/ghalym/databasesexample/SqliteExample/Presenter/NotePresenter.kt
+++ app/src/main/java/com/ghalym/databasesexample/SqliteExample/Presenter/NotePresenter.kt
@@ -2,21 +2,46 @@ package com.ghalym.databasesexample.SqliteExample.Presenter
 
 import android.content.Context
 import com.ghalym.databasesexample.R
-import com.ghalym.databasesexample.SqliteExample.data.DbConnection
+import com.ghalym.databasesexample.SqliteExample.data.NoteRepository
 import com.ghalym.databasesexample.SqliteExample.data.Note
 import com.ghalym.databasesexample.SqliteExample.ui.INoteView
 
-class NotePresenter(val iNotePresenter: INotePresenter, val context: Context) {
+class NotePresenter(val iNoteView: INoteView, val context: Context) {
 
-    val dbConnection: DbConnection;
+    val noteRepository: NoteRepository;
 
     init {
-        dbConnection = DbConnection(context, "db", null, 1);
+        noteRepository = NoteRepository(context, "db", null, 1);
     }
 
     fun addNote(note: Note) {
-        val result = dbConnection.insertNoteToDb(note);
-        if (result) iNotePresenter.onAddSuccess() else iNotePresenter.onError(context.getString(R.string.insert_error))
+        iNoteView.onShowLoading()
+        val result = noteRepository.insertNoteToDb(note);
+        if (result) iNoteView.onAddSuccess() else iNoteView.onError(context.getString(R.string.insert_error))
+    }
+
+    fun removeNoteFromDb(note: Note) {
+        iNoteView.onShowLoading()
+        val result = noteRepository.removeNoteFromDb(note);
+        if (result) iNoteView.onDeletSuccess() else iNoteView.onError(context.getString(R.string.delete_note_error))
+    }
+
+    fun getAllNote(note: Note) {
+        iNoteView.onShowLoading()
+        val result = noteRepository.getAllNote();
+        iNoteView.onFetchData(result);
+    }
+
+    fun updateNote(note: Note) {
+        iNoteView.onShowLoading()
+        val result = noteRepository.updateNote(note);
+        if (result) iNoteView.onEditSuccess() else iNoteView.onError(context.getString(R.string.edit_note_error))
+    }
+
+    fun deleteAllNotes() {
+        iNoteView.onShowLoading()
+        val result = noteRepository.deleteAllNotes();
+        if (result) iNoteView.onDeleteAllSuccess() else iNoteView.onError(context.getString(R.string.delete_allNotes_error))
     }
 
 
--- app/src/main/java/com/ghalym/databasesexample/SqliteExample/data/NoteRepository.kt
+++ app/src/main/java/com/ghalym/databasesexample/SqliteExample/data/NoteRepository.kt
@@ -1,10 +1,114 @@
 package com.ghalym.databasesexample.SqliteExample.data
 
-import com.ghalym.databasesexample.SqliteExample.Presenter.INotePresenter
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.os.Build
+import androidx.annotation.RequiresApi
 
-class NoteRepository(iNotePresenter: INotePresenter){
+final class NoteRepository(context: Context?, name: String?, factory: SQLiteDatabase.CursorFactory?, version: Int) :
+    SQLiteOpenHelper(context, name, factory, version) {
+    private val TABLE_NOTE_NAME = "Note";
+    private val TABLE_NOTE_COLUMN_ID_NAME = "id";
+    private val TABLE_NOTE_COLUMN_TITLE_NAME = "title";
+    private val TABLE_NOTE_COLUMN_CONTENT_NAME = "content";
 
 
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL("create table $TABLE_NOTE_NAME ($TABLE_NOTE_COLUMN_ID_NAME integer primary key autoincrement ,$TABLE_NOTE_COLUMN_TITLE_NAME text,$TABLE_NOTE_COLUMN_CONTENT_NAME text)")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
+        db?.execSQL("drop table IF exists $TABLE_NOTE_NAME")
+        onCreate(db)
+    }
+
+    var db: SQLiteDatabase? = null;
+
+    fun openDb() {
+        db = writableDatabase
+    }
+
+    fun closeDb() {
+        db?.close();
+    }
+
+    fun insertNoteToDb(note: Note): Boolean {
+        var contentValues = ContentValues();
+        contentValues.put("$TABLE_NOTE_COLUMN_TITLE_NAME", note.title);
+        contentValues.put("$TABLE_NOTE_COLUMN_CONTENT_NAME", note.content);
+
+        openDb()
+        val result = db?.insert("$TABLE_NOTE_NAME", null, contentValues);
+        closeDb()
+        if (!result!!.equals(-1)) {
+            return true;
+        }
+        return false;
+    }
+
+    fun removeNoteFromDb(note: Note): Boolean {
+        val arg = arrayOf(note.id.toString());
+        openDb()
+        val result = db?.delete("$TABLE_NOTE_NAME", "$TABLE_NOTE_COLUMN_ID_NAME =? ", arg);
+        closeDb()
+
+        if (result != 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    fun getAllNote(): ArrayList<Note> {
+        val allNotes = ArrayList<Note>()
+        openDb()
+        val curser = db?.rawQuery("select * from $TABLE_NOTE_NAME", null);
+        closeDb()
+
+        while (curser!!.moveToNext()) {
+
+            val id = curser.getInt(curser.getColumnIndex("$TABLE_NOTE_COLUMN_ID_NAME"));
+            val title = curser.getString(curser.getColumnIndex("$TABLE_NOTE_COLUMN_TITLE_NAME"));
+            val content = curser.getString(curser.getColumnIndex("$TABLE_NOTE_COLUMN_CONTENT_NAME"));
+
+            val note = Note(id, title, content);
+            allNotes.add(note);
+        }
+
+        return allNotes;
+
+    }
+
+    fun deleteAllNotes(): Boolean {
+        openDb()
+        val result = db?.delete("$TABLE_NOTE_NAME", null, null);
+        closeDb()
+        if (result != 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    fun updateNote(note: Note): Boolean {
+
+        var contentValues = ContentValues();
+        contentValues.put("$TABLE_NOTE_COLUMN_TITLE_NAME", note.title);
+        contentValues.put("$TABLE_NOTE_COLUMN_CONTENT_NAME", note.content);
+
+        val arg = arrayOf(note.id.toString());
+
+        openDb()
+        val result = db?.update("$TABLE_NOTE_NAME", contentValues, "$TABLE_NOTE_COLUMN_ID_NAME =? ", arg);
+        closeDb()
+        if (result != 0) {
+            return true;
+        }
+
+        return false;
+    }
 
 
 }
